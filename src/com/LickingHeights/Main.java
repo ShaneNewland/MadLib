ad43ac037d52c4439ae3db9600b49f0710fa19f2
Shane <shane.newland.2023@student.lhschools.org>
2020-02-25 13:20:23 -0500

    IntelliJ Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,105 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Declare
+
+//        Phase 1
+        String Greeting; // declared a variable
+        String Name; // declared a variable
+        String favoriteFood; // declared a variable
+        String typeOfAnimal; // declared a variable
+        String fastfoodPlace; // declared a variable
+        String Celebrity; // declared a variable
+        String movieName; // declared a variable
+        String showName; // declared a variable
+        String Country; // declared a variable
+        String State; // declared a variable
+        String Year; // declared a variable
+        String Sport; // declared a variable
+        String Athlete; // declared a variable
+        String musicalArtist; // declared a variable
+        String favoriteRapper; // declared a variable
+        String Activity; // declared a variable
+        String College; // declared a variable
+        String sportsTeam; // declared a variable
+        String Hobby; // declared a variable
+        String typeOfSeaAnimal; // declared a variable
+
+//        Phase 2
+        Scanner Keyboard;
+        Keyboard = new Scanner(System.in);
+
+        System.out.println("Type of greeting");
+        Greeting = Keyboard.nextLine();
+
+        System.out.println("What is your name?");
+        Name = Keyboard.nextLine();
+
+        System.out.println("Your favorite food?");
+        favoriteFood = Keyboard.nextLine();
+
+        System.out.println("Type of animal");
+         typeOfAnimal= Keyboard.nextLine();
+
+        System.out.println("Favorite fast food place?");
+        fastfoodPlace = Keyboard.nextLine();
+
+        System.out.println("Name a celebrity");
+         Celebrity= Keyboard.nextLine();
+
+        System.out.println("A name of movie");
+         movieName= Keyboard.nextLine();
+
+        System.out.println("A name of a show");
+         showName= Keyboard.nextLine();
+
+        System.out.println("Name a country");
+         Country= Keyboard.nextLine();
+
+        System.out.println("Name a state");
+        State = Keyboard.nextLine();
+
+        System.out.println("A year");
+        Year = Keyboard.nextLine();
+
+        System.out.println("Name a sport");
+        Sport = Keyboard.nextLine();
+
+        System.out.println("Name an athlete");
+        Athlete = Keyboard.nextLine();
+
+        System.out.println("Name a musical artist");
+        musicalArtist = Keyboard.nextLine();
+
+        System.out.println("Favorite rapper");
+        favoriteRapper = Keyboard.nextLine();
+
+        System.out.println("Favorite acvtivity to do");
+        Activity = Keyboard.nextLine();
+
+        System.out.println("Name a college");
+        College = Keyboard.nextLine();
+
+        System.out.println("Name a sports team");
+        sportsTeam = Keyboard.nextLine();
+
+        System.out.println("Name a hobby");
+        Hobby = Keyboard.nextLine();
+
+        System.out.println("Type of sea animal");
+        typeOfSeaAnimal = Keyboard.nextLine();
+
+        //Phase 3
+
+
+
+
+
+
+
+    }
+}
